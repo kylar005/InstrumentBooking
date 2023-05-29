d8b9d7f7eb3066197229ac4627aeb8f137ef39fb
Kylar <kylarrufus005@gmail.com>
2023-05-29 14:00:49 +0200

    initial

---
+++ src/main/java/com/example/instrumentbooking/InstrumentBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.instrumentbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/instrumentbooking/InstrumentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instrumentbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
